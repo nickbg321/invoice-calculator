18e365620474e554f286677685556468cd4ccbe7
Nikolay Ivanov <nikolay.ivanov321@gmail.com>
2022-12-01 23:00:09 +0200

    initial commit

---
+++ src/main/java/com/dt/invoicecalculator/InvoiceCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.dt.invoicecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dt/invoicecalculator/InvoiceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.invoicecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
